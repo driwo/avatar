d326054ab7b20c94f320624ffecb0c8741e17057
Dries Wouters <dries.wouters1@student.kuleuven.be>
2023-06-28 12:18:31 +0200

    bom files

--- js/bom.js
+++ js/bom.js
@@ -1,15 +1,9 @@
 window.onload = function() {
   var timerElement = document.getElementById("timer");
-  var redLightElement = document.getElementById("red-light");
-  var boomTextElement = document.createElement("div");
-  boomTextElement.id = "boom-text";
-  boomTextElement.innerHTML = "BOOM!";
-  document.body.appendChild(boomTextElement);
-
+  var redLightElement = document.querySelector('.circle::before');
   var seconds = 9;
   var milliseconds = seconds * 1000;
   var interval;
-  var flickerInterval = 5;
 
   function updateTimer() {
     var minutes = Math.floor(milliseconds / 60000);
@@ -19,25 +13,12 @@ window.onload = function() {
 
     if (milliseconds > 0) {
       milliseconds -= 1000;
-    } else {
+    }
+    else {
       clearInterval(interval);
-      window.location.href = "index.html";
+      window.location.href = "bom.html";
       redLightElement.style.display = "none";
-      boomTextElement.style.display = "block";
-
     }
   }
-
-  function startFlicker() {
-      redLightElement.classList.toggle("flicker");
-      flickerInterval -= 0.1;
-    setTimeout(function() {
-      if(milliseconds>0){
-        startFlicker();
-      }
-    }, 2000);
-  }
-
   interval = setInterval(updateTimer, 1000);
-  startFlicker();
 };
